ce747ccc5494f9cbe41d9c872a2c12d593849f79
Pepe <pepecc83@gmail.com>
2026-05-25 23:05:32 +0200

    Add logs

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,6 +9,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId:             process.env.VITE_FIREBASE_APP_ID,
 }
-
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY)
 export const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
