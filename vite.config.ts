301fbc136e5e102063d01b27037d0d000cc90bdc
Pepe <pepecc83@gmail.com>
2026-05-28 21:09:37 +0200

    FIX env

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import EnvironmentPlugin from 'vite-plugin-environment'
-import dotenv from 'dotenv'
-dotenv.config({})
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import EnvironmentPlugin from 'vite-plugin-environment'
+// import dotenv from 'dotenv'
+// dotenv.config({})
 
-export default defineConfig(({mode}: any) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-  plugins: [react(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })],
-    server: {
+// export default defineConfig(({mode}: any) => {
+//   const env = loadEnv(mode, process.cwd());
+//   return {
+//   plugins: [react(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })],
+//     server: {
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+//   }
+
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
     port: 5173,
     proxy: {
       '/api': {
@@ -17,6 +35,5 @@ export default defineConfig(({mode}: any) => {
       },
     },
   },
-  }
-
 })
+
