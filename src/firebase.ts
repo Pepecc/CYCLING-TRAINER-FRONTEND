f4cc774832cfcd52c60f9143d99be379cc5bc8ca
Pepe <pepecc83@gmail.com>
2026-05-28 20:57:49 +0200

    Add logs

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,6 +9,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId:             process.env.REACT_APP_FIREBASE_APP_ID,
 }
-
+console.log("API KEY:", process.env.REACT_APP_FIREBASE_API_KEY)
 export const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
