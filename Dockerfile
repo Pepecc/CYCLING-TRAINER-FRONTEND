f118534e0095dfe97e9467f7351849759addde17
Pepe <pepecc83@gmail.com>
2026-05-28 21:22:39 +0200

    Changed env

--- Dockerfile
+++ Dockerfile
@@ -24,6 +24,9 @@ COPY nginx.conf /etc/nginx/templates/default.conf.template
 
 COPY --from=builder /app/dist /usr/share/nginx/html
 
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod +x /docker-entrypoint.s
+
 EXPOSE 80
 
-CMD ["nginx", "-g", "daemon off;"]
+ENTRYPOINT ["/docker-entrypoint.sh"]
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+const env = (window as any)._env_ || {}
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:            env.VITE_FIREBASE_API_KEY,
+  authDomain:        env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             env.VITE_FIREBASE_APP_ID,
 }
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY)
+
+// console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY)
 export const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
