adecc78e96831457f90f72bc09bf9d35cdae9cd0
Pepe <pepecc83@gmail.com>
2026-05-28 18:26:38 +0200

    Fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import EnvironmentPlugin from 'vite-plugin-environment'
 import dotenv from 'dotenv'
+dotenv.config({})
 
-export default defineConfig({
- plugins: [react()],
-  server: {
+export default defineConfig(({mode}: any) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+  plugins: [react(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })],
+    server: {
     port: 5173,
     proxy: {
       '/api': {
@@ -14,4 +17,6 @@ export default defineConfig({
       },
     },
   },
+  }
+
 })
